18260baf20477dcb2e2c6c85bed9e408970e871c
kiranmychits2025 <kiranhk1102023@gmail.com>
2025-08-19 16:05:33 +0530

    group relationship manager issue resolved

--- src/instance/TokenInstance.js
+++ src/instance/TokenInstance.js
@@ -3,8 +3,8 @@ const token = localStorage.getItem("Token");
 const api = axios.create({
     // baseUrl: "https://mychits.online/api",
     // baseUrl: "http://13.61.139.208:3000/api",
-    baseURL: "http://51.21.197.152:3000/api",
-      // baseURL: "http://localhost:3000/api",
+    // baseURL: "http://51.21.197.152:3000/api",
+      baseURL: "http://localhost:3000/api",
   headers: {
     Authorization: `Bearer ${token}`,
   
